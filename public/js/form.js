758eb4664979b1dac20fa6ab58415c41e46b8ab7
maurixavi <mxavierarevalo@gmail.com>
2022-03-03 20:22:59 -0300

    signup form almost done

---
+++ public/js/form.js
@@ -0,0 +1,69 @@
+//const { response } = require("express");
+
+const loader = document.querySelector('.loader');
+
+const submitBtn = document.querySelector('.submit-btn');
+
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const number = document.querySelector('#number');
+
+const terms = document.querySelector('#terms-and-cond');
+const notification = document.querySelector('#notification');
+
+submitBtn.addEventListener('click', () => {
+    if (!name.value.length){
+        showAlert('Please enter a name');
+    } else if (name.value.length < 3){
+        showAlert('Name must be at least 3 letters long');
+    } else if (!email.value.length){
+        showAlert('Please enter an email');
+    } else if (!password.value.length){
+        showAlert('Please enter a password'); 
+    } else if (password.value.length < 8){
+        showAlert('Password should be at least 8 letters long');
+    } else if (!number.value.length){
+        showAlert('Please enter your phone number');
+    } else if (!Number(number.value) || number.length < 8){
+        showAlert('Please enter a valid number');
+    } else if (!terms.checked) {
+        showAlert('You must agree to our Terms & Conditions');
+    } else {
+        //submit form
+        loader.style.display = 'block';
+        /*sendData('/signup', {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            number = number.value,
+            terms: terms.checked,
+            notification: notification.checked,
+            seller: false
+        })*/
+    }
+})
+
+//send data
+/*const sendData = (path, data) => {
+    fetch(path, {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'aplication/json'}),
+        body: JSON.stringify(data)
+    }).then((res) => res.json())
+    .then(response => {
+        console.log(response);
+    })
+}*/
+
+
+//alert function
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 2500)
+}
--- server.js
+++ server.js
@@ -11,18 +11,26 @@ const app = express();
 
 //middlewares
 app.use(express.static(staticPath));
+//app.use(express.json());
 
 //routes
 
 //home
 app.get("/", (req, res) => {
+    //sendFile: Sets the Content-Type response HTTP header field based on the filename’s extension
     res.sendFile(path.join(staticPath, "index.html"));
 })
 
 //signup
-app.get("/signup", (req, res) =>{
+app.get("/signup", (req, res) => {
     res.sendFile(path.join(staticPath, "signup.html"));
 })
+
+/*
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    res.json('data received')
+})*/
 
 //404
 app.get("/404", (req, res) => {
