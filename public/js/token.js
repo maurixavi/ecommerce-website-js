d4902a8247676cb6f68404b408085227dc8a6e4e
maurixavi <mxavierarevalo@gmail.com>
2022-03-06 03:32:53 -0300

    comparetoken fix

--- public/js/token.js
+++ public/js/token.js
@@ -12,10 +12,9 @@ const generateToken = (key) => {
     return token;
 }
 
-
 const compareToken = (token, key) => {
     let string = '';
-    for(let i = 0; i < key.length; i=i+2){
+    for(let i = 0; i < token.length; i=i+2){
         let idx1 = char.indexOf(token[i]);
         let idx2 = char.indexOf(token[i+1]);
         string += char[idx1 + idx2];
