a4b65eaab56a976a39b15e0390a2b5ce4702c1bf
maurixavi <mxavierarevalo@gmail.com>
2022-03-06 03:40:37 -0300

    redirect to home if logged in

--- public/js/form.js
+++ public/js/form.js
@@ -1,4 +1,12 @@
-//const { response } = require("express");
+//redirect to home page if user logged in
+window.onload = () => {
+    if(sessionStorage.user){
+        user = JSON.parse(sessionStorage.user);
+        if(compareToken(user.authToken, user.email)){
+            location.replace('/');
+        }
+    }
+}
 
 const loader = document.querySelector('.loader');
 
